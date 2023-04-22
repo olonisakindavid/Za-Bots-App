7d4a914373150af978f632ffb9bb86072d81f279
Olonisakin David <olonisakin.david@stu.cu.edu.ng>
2023-04-22 22:36:43 +0100

    first commit

---
+++ src/robots.js
@@ -0,0 +1,62 @@
+export const robots = [
+  {
+    id: 1,
+    name: "Djonesplays",
+    username: "David",
+    email: "olonisakin.david@stu.cu.edu.ng",
+  },
+  {
+    id: 2,
+    name: "Hunterzolomo",
+    username: "Oguibe Favour",
+    email: "favour.oguibe@stu.cu.edu.ng",
+  },
+  {
+    id: 3,
+    name: "Quellcrist",
+    username: "Henry Joseph",
+    email: "henry.joseph@stu.cu.edu.ng",
+  },
+  {
+    id: 4,
+    name: "Laja B!",
+    username: "Laja",
+    email: "adelaja.samuel@stu.cu.edu.ng",
+  },
+  {
+    id: 5,
+    name: "6ixP",
+    username: "Nwokolo Paul",
+    email: "paul.nwokolo@stu.cu.edu.ng",
+  },
+  {
+    id: 6,
+    name: "PizzyP",
+    username: "Nwokolo Peter",
+    email: "nwokolo.peter@stu.cu.edu.ng",
+  },
+  {
+    id: 7,
+    name: "NightWing",
+    username: "Egbanubi",
+    email: "egbanubi.victor@stu.cu.edu.ng",
+  },
+  {
+    id: 8,
+    name: "Tommy",
+    username: "Tomisin",
+    email: "olaiya.tomisin@stu.cu.edu.ng",
+  },
+  {
+    id: 9,
+    name: "B2-Bot",
+    username: "B2me",
+    email: "Chaim_McDermott@dana.io",
+  },
+  {
+    id: 10,
+    name: "OseDreams",
+    username: "Ose",
+    email: "okoduwa.osemudiamen@stu.cu.edu.ng",
+  },
+];
